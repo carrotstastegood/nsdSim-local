7f35d2664eb12c915e53203ab17c9ad48f8df58b
carrotstastegood <22matti.kivinen@gmail.com>
2025-08-28 16:39:34 -0400

    Working on app

--- code/app.py
+++ code/app.py
@@ -18,27 +18,47 @@ def dprint(s):
         print(s)
 
 class main(cmd.Cmd):
-    intro = "nsdSim app. \nType help to list commands."
-    prompt = "> "
+    intro = "nsdSim app. \nType help or ? to list commands."
+    prompt = "nsdSCLI > "
 
-    def do_sim(self, arg):
+    def do_sim(self, arg): # Load simulator.
         if arg == "-i":
             subprocess.run([sys.executable, "code/sim/issue.py"])
             quit()
         elif arg == "-f":
             exec(open("code/sim/federal.py").read())
 
-    def do_about(self, arg):
+    def do_about(self, arg): # Display information about the app.
         print("nsdSim (Nationstates Democracy Simulator) is an unoficial tool to simulate elections.")
-        dprint("File: /code/app.py | Running from /")
+        dprint("File: /code/app.py | Running from /nsdSim.py")
 
-    def do_clear(self, arg):
+    def do_account(self, arg):
+        if arg == "-a":
+            print("Account information:")
+            print(a)
+        elif arg == "-s":
+            print("Settings:")
+            print(p)
+        elif arg == "-u":
+            print("User information:")
+            print(a)
+            print(p)
+
+    def do_settings(self, arg):
+        new = input("> ")
+        if arg == "debug":
+            if new not in ["True", "False"]:
+                print("Must be boolean (True, False)")
+        p[arg] = new
+        print(f"{arg} set to {new}")
+
+    def do_clear(self, arg): # Clear.
         os.system("clear")
 
-    def do_exit(self, arg):
+    def do_exit(self, arg): # Exit.
         exit()
 
-    def do_restart(self, arg):
+    def do_restart(self, arg): # Restart the app.
         exec(open("nsdSim.py").read())
 
     # Help
@@ -48,6 +68,12 @@ class main(cmd.Cmd):
 
     def help_clear(self):
         print("Clears terminal.")
+
+    def help_settings(self):
+        print("Changes settings. \n Input what setting you would like to change, and what to.")
+
+    def help_account(self):
+        print("Display account information \n  -a | Account info \n  -s | Settings \n  -u | All")
 
 if __name__ == "__main__":
     main().cmdloop()
